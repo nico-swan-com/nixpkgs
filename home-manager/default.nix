9bbd2be6dc5d505238e3889b9237ca7ea1d1a604
Nico Swan <nico@bcbgroup.io>
2024-09-24 16:15:20 +0200

    updated home manager

--- home-manager/default.nix
+++ home-manager/default.nix
@@ -11,43 +11,15 @@
 
       zsh-shell.enable = true;
 
-      # Set /etc/zshrc
-      # zsh = {
-      #   enable = true;
-      #   shellAliases = {
-      #     nix-shell = "nix-shell --run zsh";
-      #     la = "eza  --long -a --group-directories-first --icons=always --color=auto --almost-all --time-style=long-iso";
-      #     ll = "la --long --no-user --no-time --no-permissions --no-filesize";
-      #     cat = "bat -p";
-      #     grep = "grep --color=auto";
-      #     egrep = "egrep --color=auto";
-      #     fgrep = "fgrep --color=auto";
-      #   };
-      #   enableCompletion = true;
-      #   autosuggestion.enable = true;
-      #   history = {
-      #     size = 10000;
-      #     path = "${config.xdg.dataHome}/zsh/history";
-      #     expireDuplicatesFirst = true;
-      #   };
-      #   historySubstringSearch.enable = true;
-      #   syntaxHighlighting.enable = true;
-
-      #   sessionVariables = {
-      #     EDITOR = "vi";
-      #     SOPS_AGE_KEY_FILE = "${config.xdg.dataHome}/.config/sops/age/keys.txt";
-      #   };
-      # };
-
-      # # Git configuration
-      # git = {
-      #   enable = true;
-      #   userName = cfg.fullname;
-      #   userEmail = cfg.email;
-      #   extraConfig = {
-      #     init.defaultBranch = "main";
-      #   };
-      # };
+      # Git configuration
+      git = {
+        enable = true;
+        userName = cfg.fullname;
+        userEmail = cfg.email;
+        extraConfig = {
+          init.defaultBranch = "main";
+        };
+      };
 
       direnv = {
         enable = true;
